b98bb731f4f008318538de641a701b2b9e865217
thejas-1999 <thejask074@gmail.com>
2024-03-07 15:23:49 +0530

    study about post and routing parameters

--- router.js
+++ router.js
@@ -8,4 +8,24 @@ route.get("/accounts", (req, res) => {
   res.json({ userData: accounts });
 });
 
+//Post request
+
+route.post("/accounts", (req, res) => {
+  const incomingAccounts = req.body;
+  accounts.push(incomingAccounts);
+  res.json(accounts);
+});
+
+// Get account by id
+route.get("/accounts/:id", (req, res) => {
+  const parseId = parseInt(req.params.id);
+  const getAccount = accounts.find((account) => account.id === parseId);
+
+  if (!getAccount) {
+    res.status(404).send("Account not found");
+  } else {
+    res.json({ userData: [getAccount] });
+  }
+});
+
 module.exports = route;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 
 const app = express();
 
 const route = require("./router");
 
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use("/api", route);
 
